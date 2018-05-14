f523479ed7fbc2514c24e2beb3791227369e6370
Robert Tabares <robtabares92@gmail.com>
2018-05-14 19:38:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+
+def prime?(n)
+  if n<=1
+    return false
+  end
+
+  arr = (2..n-1).to_a
+  for num in arr do
+    if n%num == 0
+      return false
+    end
+  end
+  return true
+end

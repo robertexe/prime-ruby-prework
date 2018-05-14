8d9ef6076349dc2ade713ee70d8c920ef633de32
Robert Tabares <robtabares92@gmail.com>
2018-05-14 19:41:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,27 @@
 # Add  code here!
 
 
+# def prime?(n)
+#   if n<=1
+#     return false
+#   end
+# 
+#   arr = (2..n-1).to_a
+#   for num in arr do
+#     if n%num == 0
+#       return false
+#     end
+#   end
+#   return true
+# end
+
+
 def prime?(n)
   if n<=1
     return false
   end
 
-  arr = (2..n-1).to_a
+  arr = (2..Math.sqrt(n)).to_a ## cutting half of checks
   for num in arr do
     if n%num == 0
       return false
